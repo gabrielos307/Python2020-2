f6ffa22893082c228fd76154e8042e0c484dcca4
gabrielos307 <gabrielos307@gmail.com>
2020-01-04 01:28:20 -0600

    Mi primer commit

---
+++ PythonBásico/funciones.py
@@ -0,0 +1,27 @@
+'''
+print('Va entrar una funcion')
+
+def soy_una_funcion(): #para que se den cuenta que pueden nombrarla
+	print('Entro a la funcion')
+	a = 2
+	b = 3
+	c = a + b
+	print(c)
+
+soy_una_funcion()
+print('Soy otra vez el main')
+'''
+#Validar de que entra a una funcion
+valida = False
+
+
+funcion(valida)
+print(valida)
+def funcion(valida):
+	valida = True
+	print(valida)
+	return valida
+if(valida == False):
+	print('No entro la funcion')
+else:
+	print('Entro la funcion')
